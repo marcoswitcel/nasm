0d87a68956e2aa0977673738a071638e46856b94
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-10-17 22:07:22 -0300

    argumentos da linha de comando

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,11 +8,13 @@ run_nasm_linux_sample() {
 
   nasm -f elf $FILE_NAME.asm -o "$BUILD_DIR/$FILE_NAME.o"
   ld -m elf_i386 "$BUILD_DIR/$FILE_NAME.o" -o "$BUILD_DIR/$FILE_NAME"
-  "$BUILD_DIR/$FILE_NAME"
+  "$BUILD_DIR/$FILE_NAME" ${@:2}
 }
 
 # Referências
 # https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
+# https://stackoverflow.com/questions/6212219/passing-parameters-to-a-bash-function
+# https://unix.stackexchange.com/questions/82060/bash-slice-of-positional-parameters
 
 echo "-----------------------------------"
 echo "| Now you are playing with power! |"
