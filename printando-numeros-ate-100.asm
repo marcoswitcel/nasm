5d874ced603b4a6c54b3df04d19cf8920d0f4eb4
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-10-21 23:41:51 -0300

    printa números até 100, completo

--- printando-numeros-ate-100.asm
+++ printando-numeros-ate-100.asm
@@ -4,6 +4,33 @@ mensagem db 'ola mundo', 0Ah, 0h
 SECTION .text
 global _start
 
+printa_integer:
+    push eax   ; Preâmbulo
+    push ecx
+    push edx
+    push esi
+    mov ecx, 0 ; Inicializando ECX para 0
+push_chars_loop:
+    inc ecx ; Incrementando contador de chars que foram empurrados para a stack
+    mov edx, 0  ; resetnado EDX
+    mov esi, 10 ; movendo 10 para o ESI
+    idiv esi    ; dividindo EAX por ESI, armazenando a quantidade de vezes que EAX coube em ESI dentro de EAX e armazenando o resto em EDX
+    add edx, 48 ; adicionado 48 ao inteiro armazenado em EDX, que agora sabemos estar entre 0 e 9, portanto adicionando 48 teremos o código do seu char na tabela ASCII 
+    push edx
+    cmp eax, 0  ; ainda conseguimos dividí-lo?
+    jnz push_chars_loop
+printa_char_loop:
+    dec ecx   ; decrementa o contador criado anteriormente
+    pop eax
+    call printa_char
+    cmp ecx, 0
+    jnz printa_char_loop
+    pop esi
+    pop edx
+    pop ecx
+    pop eax
+    ret
+
 printa_mensagem_padrao:
     push edx
     push ecx
@@ -38,18 +65,15 @@ printa_char:
 
 _start:
     call printa_mensagem_padrao    
-    mov ecx, 11
+    mov eax, 0
 loop:
-    dec ecx,
-    mov eax, ecx
-    dec eax
+    inc eax
     push eax
-    add eax, 48
-    call printa_char
+    call printa_integer
     mov eax, 0Ah
     call printa_char
     pop eax
-    cmp eax, 0
+    cmp eax, 100
     jnz loop 
 
 
